db7fe0f16ad233af590a74205f8ae2af3be0996b
Nick <n.sakharov@icloud.com>
2025-06-01 19:11:32 +0700

    Use ngFor for displaying buttons

--- src/app/mycalc/mycalc.component.ts
+++ src/app/mycalc/mycalc.component.ts
@@ -1,13 +1,36 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import { subscriptionLogsToBeFn } from 'rxjs/internal/testing/TestScheduler';
 
 @Component({
   selector: 'app-mycalc',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgFor, NgClass],
   templateUrl: './mycalc.component.html',
   styleUrl: './mycalc.component.css',
 })
 export class MycalcComponent implements OnInit {
+  buttonValues: string[] = [
+    'AC',
+    '÷',
+    '*',
+    'C',
+    '1',
+    '2',
+    '3',
+    '-',
+    '4',
+    '5',
+    '6',
+    '+',
+    '7',
+    '8',
+    '9',
+    '=',
+    '.',
+    '0',
+    '00',
+  ];
   inputStr: any;
   ngOnInit(): void {
     this.inputStr = new FormGroup({ display: new FormControl('') });
