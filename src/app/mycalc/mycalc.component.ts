2e7a9f2c6f57a287f776928a39e2bba2ba71e966
Nick <n.sakharov@icloud.com>
2025-06-01 21:26:04 +0700

    Modified buttonDown function

--- src/app/mycalc/mycalc.component.ts
+++ src/app/mycalc/mycalc.component.ts
@@ -11,6 +11,7 @@ import { subscriptionLogsToBeFn } from 'rxjs/internal/testing/TestScheduler';
 })
 export class MycalcComponent implements OnInit {
   operators = new Set([0, 1, 2, 3, 7, 11, 15]);
+  operatorsSigns = new Set(['+', '-', '*', '÷']);
   inputStr: any;
 
   buttonValues: string[] = [
@@ -39,23 +40,36 @@ export class MycalcComponent implements OnInit {
     this.inputStr = new FormGroup({ display: new FormControl('') });
   }
 
-  buttonDown(buttonElement: any) {
-    let buttonText = buttonElement;
-    if (this.inputStr.display != '') {
-      this.inputStr.display.setValue(this.inputStr.display.value + buttonText);
-    } else {
-      this.inputStr.display.setValue(buttonText);
+  buttonDown(buttonElement: string): void {
+    const displayControl = this.inputStr.get('display');
+    const currentValue = displayControl?.value || '';
+    if (buttonElement === 'AC') {
+      displayControl?.setValue('');
+      return;
     }
-  }
+    if (buttonElement === 'C' && currentValue > 0) {
+      displayControl?.setValue(currentValue.slice(0, -1));
+      return;
+    }
 
-  clearDisplay() {
-    this.inputStr.display = '';
+    if (
+      this.operatorsSigns.has(buttonElement) &&
+      this.operatorsSigns.has(currentValue.slice(-1))
+    ) {
+      return;
+    }
+    displayControl?.setValue(currentValue + buttonElement);
   }
 
   getButtonClasses(i: number) {
-    return [
-      this.operators.has(i) ? 'bg-red-500' : 'bg-green-500',
-      i === 15 ? 'row-span-2' : 'row-span-1',
-    ];
+    // return [
+    //   this.operators.has(i) ? 'bg-red-500' : 'bg-green-500',
+    //   i === 15 ? 'row-span-2' : 'row-span-1',
+    // ];
+    const operators = this.operators.has(i) ? 'bg-red-500' : 'bg-green-500';
+    const equalSign = i === 15 ? 'row-span-2' : 'row-span-1';
+    const base = 'rounded-2xl cursor-pointer';
+
+    return `${base} ${operators} ${equalSign}`;
   }
 }
